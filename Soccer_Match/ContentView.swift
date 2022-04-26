b279a1ff066ff3d5135bd90528ba154d29563f8c
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-04-26 16:32:08 +0900

    Initial Commit

---
+++ Soccer_Match/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soccer_Match
+//
+//  Created by 이창형 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Soccer_Match/Soccer_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soccer_MatchApp.swift
+//  Soccer_Match
+//
+//  Created by 이창형 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Soccer_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

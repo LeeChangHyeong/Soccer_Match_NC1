19e1796f3ed722931b4665bebe301fcc1acc1813
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-05-05 18:02:36 +0900

    last

--- Soccer_Match/TeamPage/Drag_Drop.swift
+++ Soccer_Match/TeamPage/Drag_Drop.swift
@@ -7,19 +7,22 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct Drag_Drop: View {
     
     @State private var dragOffset = CGSize.zero
     @State private var dragOffset2 = CGSize.zero
     
     var body: some View {
         Image(systemName: "person.fill")
+            .font(.largeTitle)
+        // 기준이 되는 주소에 더해지는 x,y 값
             .offset(x: dragOffset.width , y:dragOffset.height )
             .gesture(DragGesture()
                 .onChanged { gesture in
                     dragOffset = CGSize(width: gesture.translation.width + dragOffset2.width, height: gesture .translation.height + dragOffset2.height)
                     
-                }
+                }    //함수
+                     //온체인지가 함수를 입력값으로 받는 메소드
                 .onEnded { gesture in
                     dragOffset = CGSize(width: gesture.translation.width + dragOffset2.width, height: gesture .translation.height + dragOffset2.height)
                     dragOffset2 = dragOffset
@@ -27,8 +30,8 @@ struct SwiftUIView: View {
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct Drag_Drop_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        Drag_Drop()
     }
 }
